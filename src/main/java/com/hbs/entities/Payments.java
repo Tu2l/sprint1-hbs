c82ffaae954e4a35e8382577ad12ba006d70e0c1
Guna <gunasekhar.m@capgemini.com>
2023-01-19 15:34:28 +0530

    first commit

--- src/main/java/com/hbs/entities/Payments.java
+++
@@ -1,7 +0,0 @@
-package com.hbs.entities;
-
-public class Payments {
-	private int payment_id;
-	private int booking_id;
-	private int transaction_id;
-}
--- src/main/java/com/hbs/service/IAdminService.java
+++
@@ -1,9 +0,0 @@
-package com.hbs.service;
-
-import com.hbs.entities.Admin;
-
-public interface IAdminService {
-	public Admin signIn(Admin admin);
-	public Admin signOut(Admin admin);
-	
-}
