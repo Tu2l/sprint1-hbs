20f7fd7ad2fcf2342bc12629f7fbbdc887d2338f
tutroy <tutroy@INLEN8520017313.corp.capgemini.com>
2023-01-19 17:05:06 +0530

    restore

--- src/main/java/com/cg/hbs/entites/Admin.java
+++
@@ -1,8 +0,0 @@
-package com.cg.hbs.entites;
-
-public class Admin {
-	private int admin_id;
-	private String admin_name;
-	private String password;
-	
-}
--- src/main/java/com/cg/hbs/entites/Payments.java
+++
@@ -1,7 +0,0 @@
-package com.cg.hbs.entites;
-
-public class Payments {
-	private int payment_id;
-	private int booking_id;
-	private int transaction_id;
-}
