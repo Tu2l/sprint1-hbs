03d1aea2251e32995db212a5c0d2822a74b2a98c
durges <durges@lin80020560.corp.capgemini.com>
2023-01-19 16:11:56 +0530

    transaction

---
+++ src/main/java/com/cg/hbs/entites/Payments.java
@@ -0,0 +1,7 @@
+package com.cg.hbs.entites;
+
+public class Payments {
+	private int payment_id;
+	private int booking_id;
+	private int transaction_id;
+}
---
+++ src/main/java/com/cg/hbs/entites/User.java
@@ -0,0 +1,11 @@
+package com.cg.hbs.entites;
+
+public class User {
+	private int user_id;
+	private String user_name;
+	private String email;
+	private String password;
+	private String role;
+	private String mobile;
+	private String address;
+}
