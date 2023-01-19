457d4aac46bb0ab8edee7cf0ef54a6543909fcd2
tutroy <tutroy@INLEN8520017313.corp.capgemini.com>
2023-01-19 17:13:50 +0530

    Booking details added

--- src/main/java/com/hbs/entities/BookingDetails.java
+++ src/main/java/com/hbs/entities/BookingDetails.java
@@ -1,15 +1,58 @@
 package com.hbs.entities;
 
 import java.util.Date;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Entity
+@Table(name = "booking_details_table")
 public class BookingDetails {
-	private int booking_id;
-	private int hotel_id;
-	private int room_id;
-	private int user_id;
-	private Date booked_from;
-	private Date booked_to;
-	private int no_of_adults;
-	private int no_of_children;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int bookingId;
+
+	@Column(name = "user_id")
+	private User userId;
+
+	@Column(name = "hotel_id", nullable = false, unique = true)
+	private Hotel hotelId;
+
+	@Column(name = "room_id", nullable = false, unique = true)
+	private User roomId;
+
+	@Column(name = "booked_from", nullable = false, unique = true)
+	private Date bookedFrom;
+
+	@Column(name = "booked_to", nullable = false, unique = true)
+	private Date bookedTo;
+
+	@Column(name = "no_of_adults", nullable = false, unique = true)
+	private int noOfAdults;
+
+	@Column(name = "no_of_children", nullable = false, unique = true)
+	private int noOfChildren;
+
+	@Column(name = "amount", nullable = false)
 	private double amount;
+
 }
