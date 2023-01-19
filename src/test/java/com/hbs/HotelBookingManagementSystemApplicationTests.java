5a5455dc2b0d2dd4644c220e492027992ae6a7a2
Roy <tutul.roy@capgemini.com>
2023-01-19 12:11:42 +0530

    project structure

---
+++ src/test/java/com/hbs/HotelBookingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
