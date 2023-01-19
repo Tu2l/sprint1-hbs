d4f2c85bff4854e87e52c040f8848a2b90f49e46
saurabh <sourabhshedbale77@gmail.com>
2023-01-19 19:24:47 +0530

    commit

--- src/main/java/com/hbs/HotelBookingManagementSystemApplication.java
+++ src/main/java/com/hbs/HotelBookingManagementSystemApplication.java
@@ -9,6 +9,8 @@ public class HotelBookingManagementSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HotelBookingManagementSystemApplication.class, args);
 		System.out.println("Running");
+		System.err.println("Running");
+		
 	}
 
 }
---
+++ src/main/java/com/hbs/entities/User.java
@@ -0,0 +1,57 @@
+package com.hbs.entities;
+
+import javax.persistence.Column;
+
+import javax.persistence.Entity;
+
+import javax.persistence.GeneratedValue;
+
+import javax.persistence.GenerationType;
+
+import javax.persistence.Id;
+
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+
+import lombok.Data;
+
+import lombok.EqualsAndHashCode;
+
+import lombok.NoArgsConstructor;
+
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Entity
+@Table(name = "user_details")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "user_id", unique = true, nullable = true)
+	private int userId;
+
+	@Column(name = "user_name", nullable = true)
+	private String userName;
+
+	@Column(name = "email", unique = true, nullable = true)
+	private String email;
+
+	@Column(name = "password", unique = true, nullable = true)
+	private String password;
+
+	@Column(name = "role", nullable = true)
+	private String role;
+
+	@Column(name = "mobile", unique = true, nullable = true)
+	private String mobile;
+
+	@Column(name = "address", nullable = true)
+	private String address;
+
+}
