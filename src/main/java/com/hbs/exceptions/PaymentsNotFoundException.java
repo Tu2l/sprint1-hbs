32ed84c5d5b8463d8309c6a3000bddaca83566b9
tutroy <tutroy@INLEN8520017313.corp.capgemini.com>
2023-01-20 10:15:01 +0530

    global exception handler added

--- src/main/java/com/hbs/exceptions/PaymentsNotFoundException.java
+++ src/main/java/com/hbs/exceptions/PaymentsNotFoundException.java
@@ -1,4 +1,12 @@
 package com.hbs.exceptions;
-public class PaymentsNotFoundException extends Exception {
 
+public class PaymentsNotFoundException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2451583801836243506L;
+
+	public PaymentsNotFoundException(String message) {
+		super(message);
+	}
 }
