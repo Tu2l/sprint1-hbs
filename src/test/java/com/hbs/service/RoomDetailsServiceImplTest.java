eb0c0d714e34b714ed9950cc9044485240304d1f
Nagarjuna Reddy Cheppala <arjun46214@gmail.com>
2023-01-21 12:30:36 +0530

    testcases added for services

---
+++ src/test/java/com/hbs/service/RoomDetailsServiceImplTest.java
@@ -0,0 +1,14 @@
+package com.hbs.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RoomDetailsServiceImplTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
