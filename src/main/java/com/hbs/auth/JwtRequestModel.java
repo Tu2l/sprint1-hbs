c1c026682000492eb99c83d6098d1eb4c3c3bbf7
tutroy <tutroy@INLEN8520017313.corp.capgemini.com>
2023-01-21 09:23:53 +0530

    jwt added

---
+++ src/main/java/com/hbs/auth/JwtRequestModel.java
@@ -0,0 +1,17 @@
+package com.hbs.auth;
+
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtRequestModel {
+	@NotNull(message = "username/email must not be null")
+	private String userName;
+	@NotNull(message = "password must not be null")
+	private String password;
+}
