88aa336b6d4a937c0700a2e23d51f8defe4a4c9b
tutroy <tutroy@INLEN8520017313.corp.capgemini.com>
2023-01-21 10:14:16 +0530

    admin dto added

--- src/main/java/com/hbs/repository/HotelRepository.java
+++ src/main/java/com/hbs/repository/HotelRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.hbs.entities.Hotel;
 
 @Repository
-public interface IHotelRepository extends JpaRepository<Hotel, Integer> {
+public interface HotelRepository extends JpaRepository<Hotel, Integer> {
 	
 }
