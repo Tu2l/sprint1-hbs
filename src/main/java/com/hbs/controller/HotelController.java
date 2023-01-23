702253debab8bd2e90eded275236e0020f28c686
tutroy <tutroy@INLEN8520017313.corp.capgemini.com>
2023-01-23 18:33:40 +0530

    Merge branch 'main' of https://github.com/Tu2l/sprint1-hbs.git into tutul/validation

--- src/main/java/com/hbs/controller/HotelController.java
+++ src/main/java/com/hbs/controller/HotelController.java
@@ -17,13 +17,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hbs.dto.HotelDTO;
-import com.hbs.entities.Hotel;
 import com.hbs.exceptions.HotelAlreadyExistsExcetion;
 import com.hbs.exceptions.HotelNotFoundException;
 import com.hbs.exceptions.InvalidEmailFormatException;
 import com.hbs.exceptions.InvalidMobileNumberFormatException;
 import com.hbs.service.HotelService;
-import com.hbs.util.MapperUtil;
 
 @RestController
 @RequestMapping("/hotel")
@@ -34,7 +32,6 @@ public class HotelController {
 	@PostMapping
 	public ResponseEntity<HotelDTO> add(@Valid @RequestBody HotelDTO dto)
 			throws InvalidEmailFormatException, InvalidMobileNumberFormatException, HotelAlreadyExistsExcetion {
-		//Hotel newHotel = hotelService.add(MapperUtil.mapToHotel(hotelDto));
 		return new ResponseEntity<>(hotelService.add(dto), HttpStatus.CREATED);
 	}
 
@@ -43,27 +40,23 @@ public class HotelController {
 			throws HotelNotFoundException, InvalidEmailFormatException, InvalidMobileNumberFormatException,
 			HotelAlreadyExistsExcetion {
 		
-//		Hotel updateHotel = hotelService.update(MapperUtil.mapToHotel(hotelDto));
-//		updateHotel.setHotelId(id);
+		dto.setHotelId(id);
 		
 		return new ResponseEntity<>(hotelService.update(dto), HttpStatus.OK);
 	}
 
 	@DeleteMapping("/{id}")
 	public ResponseEntity<HotelDTO> remove(@PathVariable int id) throws HotelNotFoundException {
-		//Hotel removeHotel = hotelService.remove(id);
 		return new ResponseEntity<>(hotelService.remove(id), HttpStatus.OK);
 	}
 
 	@GetMapping
 	public ResponseEntity<List<HotelDTO>> findAll() {
-		//List<Hotel> showAllHotel = hotelService.findAll();
 		return new ResponseEntity<>(hotelService.findAll(), HttpStatus.OK);
 	}
 
 	@GetMapping("/{id}")
 	public ResponseEntity<HotelDTO> findById(@PathVariable int id) throws HotelNotFoundException {
-	//	Hotel showHotel = hotelService.findById(id);
 		return new ResponseEntity<>(hotelService.findById(id), HttpStatus.OK);
 	}
 }
